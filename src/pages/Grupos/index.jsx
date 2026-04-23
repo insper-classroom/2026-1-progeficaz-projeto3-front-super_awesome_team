d6b1573a3db45459cf34c9fc5ec6bfd4fd620d32
Brenda Lima <breholiveira07@gmail.com>
2026-04-23 14:44:10 -0300

    feat: listagem e criação de grupos

--- src/pages/Grupos/index.jsx
+++ src/pages/Grupos/index.jsx
@@ -1,9 +1,12 @@
 // Página de listagem de grupos do usuário
 import { useState } from 'react'
 import styles from './Grupos.module.css'
+import { useNavigate } from 'react-router-dom'
 
 export function Grupos() {
 
+    const navigate = useNavigate() // hook para navegar entre páginas
+    const [grupos, setGrupos] = useState([]) // lista de grupos criados pelo usuario
     const [modalAberto, setModalAberto] = useState(false) // controla a visibilidade do modal
     const [nomeGrupo, setNomeGrupo] = useState('') // nome digitado para o novo grupo
     const [emailMembro, setEmailMembro ] = useState('') // e-mail digitado para adicionar membro
@@ -28,16 +31,30 @@ export function Grupos() {
       setMembros([])
     }
 
+    // cria grupo
+    function criarGrupo() {
+      if (!nomeGrupo) return 
+      const novoGrupo = { id: Date.now(), nome: nomeGrupo, membros }
+      //adiciona o novo grupoa lista e fecha modal
+      setGrupos([...grupos, novoGrupo])
+      fecharModal()
+    }
+
     return (
         <div className={styles.pagina}>
             <h1>Meus Grupos</h1>
 
             <button onClick={() => setModalAberto(true)}>Criar grupo</button>
-            {/* lista de grupos — por enquanto vazia */}
+            {/* lista de grupos criados — cada card navega para o dashboard do grupo*/}
+            
             <div>
-                <p>Nenhum grupo ainda.</p>
+                {grupos.map((grupo) => (
+                    <div key={grupo.id} onClick={() => navigate(`/grupos/${grupo.id}`)}>
+                      {grupo.nome}
+                    </div>
+                  ))}
             </div>
-
+            
             {/* modal de criar grupo — aparece quando modalAberto for true */}
             {modalAberto && (
                 <div className={styles.modal}>
@@ -68,7 +85,7 @@ export function Grupos() {
                   ))}
                 </ul>
 
-                <button className={styles.botaoCriar}>Criar</button>
+                <button className={styles.botaoCriar} onClick={criarGrupo}>Criar</button>
                 <button onClick={() => fecharModal()}>Fechar</button>
                 </div>
             )}
